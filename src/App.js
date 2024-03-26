c285ca118f9109588f05a848d5de5da5e702f1c8
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2024-03-26 18:15:32 -0400

    Added the footer with the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by
+        <a href="https://github.com/yoshino62" target="_blank" rel="noreferrer">
+          Yoshino Andrews
+        </a>{" "}
+        and open-sourced
+        <a
+          href="https://github.com/yoshino62/react-w5-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
